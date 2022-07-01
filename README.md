11cd3b5ec0a6f8511f7b0934cdb7e40992d2011e
AngeloStatescu <35111313+AngeloStatescu@users.noreply.github.com>
2022-07-01 22:45:06 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starter template for a vanilla Svelte project with Typescript and TailwindCSS
+
+### Clone and get started! 🐱‍🏍
---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1 class="m-auto text-5xl font-extrabold text-red-600">#TODO</h1>
+</main>
